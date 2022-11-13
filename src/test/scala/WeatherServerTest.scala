6c914c41cf81a00454fe628d7a3e25d382ef5f7f
neal.friedman <>
2022-11-13 16:00:43 -0700

    clean up

--- src/test/scala/WeatherServerTest.scala
+++
@@ -1,7 +0,0 @@
-import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.flatspec.AnyFlatSpecLike
-import org.scalatest.matchers.must.Matchers
-
-class WeatherServerTest extends AnyFlatSpecLike with Matchers with LazyLogging {
-
-}
